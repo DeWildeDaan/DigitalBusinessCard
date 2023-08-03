583e2a55c08b4a6b91bd2230b7a789e801992d65
Daan De Wilde <daan.de.wilde@student.howest.be>
2023-08-04 00:32:15 +0200

    finished

--- Script/app.js
+++ Script/app.js
@@ -1,19 +1,37 @@
 "use strict";
+
 //#region ***  DOM references                           ***********
 
 //#endregion
 
 //#region ***  Callback-Visualisation - show___         ***********
+function showData(respondeData) {
+  console.log(respondeData);
+}
 //#endregion
 
 //#region ***  Callback-No Visualisation - callback___  ***********
 //#endregion
 
 //#region ***  Data Access - get___                     ***********
+function getData() {
+  fetch("./Script/config.json").then((response) => {
+    return response.json().then(function (respondeData) {
+      showData(respondeData);
+    });
+  });
+}
 //#endregion
 
 //#region ***  Event Listeners - listenTo___            ***********
 //#endregion
 
 //#region ***  Init / DOMContentLoaded                  ***********
+const init = function () {
+  console.log("DOM Content loaded");
+
+  getData();
+};
+
+document.addEventListener("DOMContentLoaded", init);
 //#endregion
